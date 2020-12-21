0173f9a2375c2ded70248f8380a4e22e30b07050
Andrew Radev <andrey.radev@gmail.com>
2020-12-21 15:48:46 +0200

    Homework 3

---
+++ 03/test_basic.rs
@@ -0,0 +1,48 @@
+use solution::*;
+
+use std::io::{BufRead, BufReader};
+
+#[test]
+fn test_string_parsing() {
+    assert_eq!(skip_next("[test]", '['), Some("test]"));
+    assert_eq!(take_until("one/two", '/'), ("one", "/two"));
+    assert_eq!(take_and_skip("one/two", '/'), Some(("one", "two")));
+}
+
+#[test]
+fn test_basic_csv() {
+    let data = r#"
+        name, age, birth date
+        "Gen Z. Person", "20", "2000-01-01"
+    "#.trim().as_bytes();
+
+    let mut csv = Csv::new(BufReader::new(data)).unwrap();
+    csv.apply_selection(|_row| Ok(true));
+
+    // Parse line:
+    let row = csv.parse_line(r#""Basic Name","13","2020-01-01""#).unwrap();
+    assert_eq! {
+        (row["name"].as_str(), row["age"].as_str(), row["birth date"].as_str()),
+        ("Basic Name", "13", "2020-01-01"),
+    };
+
+    // Iteration:
+    let filtered_names = csv.map(|row| row.unwrap()["name"].clone()).collect::<Vec<_>>();
+    assert_eq!(filtered_names, &["Gen Z. Person"]);
+
+    // Writing:
+    let mut csv = Csv::new(BufReader::new(data)).unwrap();
+    csv.apply_selection(|_row| Ok(true));
+
+    let mut output = Vec::new();
+    csv.write_to(&mut output).unwrap();
+
+    let output_lines = output.lines().
+        map(Result::unwrap).
+        collect::<Vec<String>>();
+
+    assert_eq!(output_lines, &[
+        "name, age, birth date",
+        "\"Gen Z. Person\", \"20\", \"2000-01-01\"",
+    ]);
+}
