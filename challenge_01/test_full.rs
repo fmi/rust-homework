22b0d2188b0bd1033ccbb37fcd0b1e94c4a6441c
Andrew Radev <andrey.radev@gmail.com>
2021-10-17 23:24:57 +0300

    Oops. Fix test conditions

--- challenge_01/test_full.rs
+++ challenge_01/test_full.rs
@@ -4,17 +4,18 @@ use solution::*;
 
 #[test]
 fn test_full() {
-    assert_eq!(fib(0), 1);
+    assert_eq!(fib(0), 0);
     assert_eq!(fib(1), 1);
-    assert_eq!(fib(2), 1 + 1);
-    assert_eq!(fib(3), 1 + 2);
-    assert_eq!(fib(4), 2 + 3);
-    assert_eq!(fib(5), 3 + 5);
-    assert_eq!(fib(6), 5 + 8);
-    assert_eq!(fib(7), 8 + 13);
-    assert_eq!(fib(8), 13 + 21);
-    assert_eq!(fib(9), 21 + 34);
-    assert_eq!(fib(10), 34 + 55);
-    assert_eq!(fib(11), 55 + 89);
-    assert_eq!(fib(12), 89 + 144);
+    assert_eq!(fib(2), 0 + 1);
+    assert_eq!(fib(3), 1 + 1);
+    assert_eq!(fib(4), 1 + 2);
+    assert_eq!(fib(5), 2 + 3);
+    assert_eq!(fib(6), 3 + 5);
+    assert_eq!(fib(7), 5 + 8);
+    assert_eq!(fib(8), 8 + 13);
+    assert_eq!(fib(9), 13 + 21);
+    assert_eq!(fib(10), 21 + 34);
+    assert_eq!(fib(11), 34 + 55);
+    assert_eq!(fib(12), 55 + 89);
+    assert_eq!(fib(13), 89 + 144);
 }
