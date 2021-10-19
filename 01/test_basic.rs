342ff935eb5a26c57dc9d0f5af35d6b6d5965a9c
Andrew Radev <andrey.radev@gmail.com>
2021-10-19 17:13:34 +0300

    Task 1

---
+++ 01/test_basic.rs
@@ -0,0 +1,17 @@
+// Бележка: името на проекта трябва да се казва "solution". Ако не се казва така, променете го
+// на този ред:
+use solution::*;
+
+#[test]
+fn test_basic() {
+    let input: Vec<char> = "GC".chars().collect();
+    let counter = solution::counts(&input);
+
+    assert_eq!(counter.g, 1);
+    assert_eq!(counter.c, 1);
+    assert_eq!(counter.a, 0);
+    assert_eq!(counter.t, 0);
+
+    assert_eq!(solution::dna_complement(&input),         vec!['C', 'G']);
+    assert_eq!(solution::reverse_rna_complement(&input), vec!['G', 'C']);
+}
