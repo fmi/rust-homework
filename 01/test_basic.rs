2392eb9d9e9a3ea2776de9b30f48ff1d3f4536c7
Andrew Radev <andrey.radev@gmail.com>
2021-10-22 14:48:56 +0300

    Avoid unused import warning

--- 01/test_basic.rs
+++ 01/test_basic.rs
@@ -5,13 +5,13 @@ use solution::*;
 #[test]
 fn test_basic() {
     let input: Vec<char> = "GC".chars().collect();
-    let counter = solution::counts(&input);
+    let counter = counts(&input);
 
     assert_eq!(counter.g, 1);
     assert_eq!(counter.c, 1);
     assert_eq!(counter.a, 0);
     assert_eq!(counter.t, 0);
 
-    assert_eq!(solution::dna_complement(&input),         vec!['C', 'G']);
-    assert_eq!(solution::reverse_rna_complement(&input), vec!['G', 'C']);
+    assert_eq!(dna_complement(&input),         vec!['C', 'G']);
+    assert_eq!(reverse_rna_complement(&input), vec!['G', 'C']);
 }
